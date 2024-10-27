1ac7d313a22e8c37156bb911ac2451814aca5db8
i12gafej <i12gafej@uco.es>
2024-10-27 10:25:12 +0100

    Juego blackjack

---
+++ Final practice/caratula.rkt
@@ -0,0 +1,50 @@
+(require (lib "graphics.ss" "graphics"))
+
+(open-graphics)
+
+(define hor 800)
+(define ver 600)
+(define v1 (open-viewport "Ejemplo de figuras" hor ver))
+
+(define color1 (make-rgb 0.9 0.1 0.2))
+(define color2 (make-rgb 0.1 0.2 0.9))
+(define color3 (make-rgb 0.2 0.9 0.1))
+(define color4 (make-rgb 0.2 0.1 0.9))
+(define color5 (make-rgb 0.0 0.0 0.0))
+
+((draw-polygon v1)
+(list
+ 
+)(make-posn 0 0) 
+ color1)
+((draw-polygon v1)
+(list
+ 
+)(make-posn 0 0) 
+ color1)
+((draw-polygon v1)
+(list
+ 
+)(make-posn 0 0) 
+ color1)
+((draw-polygon v1)
+(list
+ 
+)(make-posn 0 0) 
+ color1)
+((draw-polygon v1)
+(list
+ 
+)(make-posn 0 0) 
+ color1)
+((draw-polygon v1)
+(list
+ 
+)(make-posn 0 0) 
+ color1)
+((draw-polygon v1)
+(list
+ 
+)(make-posn 0 0) 
+ color1)
+
---
+++ Final practice/funcionalidades.rkt
@@ -0,0 +1,8 @@
+#lang racket
+
+(require "FIGURAS_constructores.rkt")
+
+
+
+;; Llama a la función después de importar el archivo
+(dibujar-carta 200 400 0.3 'corazon lista-t)
